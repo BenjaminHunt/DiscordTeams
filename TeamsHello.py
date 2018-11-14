9cf9d1de8d6cf8a429715617af85169bd8f8c23d
Ben <bjh1381@rit.edu>
2018-11-14 15:17:28 -0500

    Refactor complete. !newteam and !newmatch work properly.

--- TeamsHello.py
+++ TeamsHello.py
@@ -6,7 +6,7 @@ from Season import Match
 from Team import Team
 
 TOKEN = sys.argv[1]
-print(TOKEN)
+print("Token: " + TOKEN)
 
 client = discord.Client()
 teams = []
@@ -28,56 +28,25 @@ async def hello(context):
 async def repeat(ctx):
     await bot.say(ctx.message.content)
 
+#!newteam <team name>, member1, member2, ..., membern
 @bot.command(pass_context=True, brief="Creates a new team with 0..n players")
 async def newteam(context):
-    array = message_to_args(context.message.content)
+    array = message_to_array(context.message.content)
     team = array[0]
     members = array[1:]
 
     team = Team(name=team, members=members)
     msg = ('New Team Created!\n' + team.to_s())
-    await client.send_message(context.message.channel, msg)
+    await bot.say(msg)
 
-'''
-@bot.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
+#!newmatch <team1>, <team2>, <mon d yyyy hh:mm(AM/PM)>, <location>
+@bot.command(pass_context=True, brief="Schedule a new match.")
+async def newmatch(context):
+    array = message_to_array(context.message.content)
+    match = Match(array[0], array[1], array[2], array[3])
+    msg = 'New Match Created!\n' + match.to_s()
+    await bot.say(msg)
 
-    array = message.content.split(",")
-    if len(array) > 1:
-        cmd = array[0][:array[0].find(' ')]
-        firstparam = array[0][array[0].find(' '):].strip()
-        array = [firstparam] + array[1:]
-    else:
-        cmd = array[0]
-
-    i = 0
-    while i < len(array):
-        array[i] = array[i].strip()
-        i += 1
-
-    print(cmd)
-    print(array)
-
-    #!newteam <team name>, member1, member2, ..., membern
-    if cmd == "!newteam":
-        team = array[0]
-        members = array[1:]
-
-        team = Team(name=team, members=members)
-        msg = ('New Team Created!\n' + team.to_s()).format(message)
-        await client.send_message(message.channel, msg)
-        #teams += [team]
-
-    #!newmatch <team1>, <team2>, <mon d yyyy hh:mm(AM/PM)>, <location>
-    elif cmd == "!newmatch":
-        match = Match(array[0], array[1], array[2], array[3])
-        msg = 'New Match Created!\n' + match.to_s()
-        await client.send_message(message.channel, msg)
-
-'''
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -92,17 +61,15 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-def message_to_args(message):
-    array = message.content.split(",")
+def message_to_array(message):
+    array = message.split(",")
     if len(array) > 1:
         firstparam = array[0][array[0].find(' '):].strip()
         array = [firstparam] + array[1:]
-
     i = 0
     while i < len(array):
         array[i] = array[i].strip()
         i += 1
-
     return array
 
 bot.run(TOKEN)

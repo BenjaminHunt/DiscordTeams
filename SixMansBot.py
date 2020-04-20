e387bc1b4dd96f30df02fbdb5f8a11d22b92d1ce
Benjamin Hunt <bjh1381@rit.edu>
2020-04-19 21:58:36 -0400

    dynamically creates home/away discord channels, moves players to those channels, then tears down voice channels upon completion

--- SixMansBot.py
+++ SixMansBot.py
@@ -1,23 +1,40 @@
 # bot.py
 
-import discord
+import asyncio
 from discord.ext import commands
 import random
-
 import sys
+
+global game
+
+
+class SixManGame:
+    def __init__(self, home, away):
+        self.home_team = home
+        self.away_team = away
+
+    async def teardown(self):
+        await self.home_team.teardown()
+        await self.away_team.teardown()
 
 
 class SixManTeam:
-    def __init__(self, voice_channel, players=[]):
+    def __init__(self, waiting_room, voice_channel, players=[]):
+        self.waiting_room = waiting_room
         self.voice_channel = voice_channel
         self.players = players
 
+    async def add_player(self, player):
+        self.players.append(player)
+        await player.move_to(self.voice_channel)
 
-class SixMans:
-    def __init__(self, players=[]):
-        self.players = players
-        self.home = []
-        self.away = []
+    def get_players(self):
+        return self.players
+
+    async def teardown(self):
+        for player in self.players:
+            await player.move_to(self.waiting_room)
+        await self.voice_channel.delete(reason="6-Mans Teardown")
 
 
 def run():
@@ -28,10 +45,9 @@ def run():
     bot = commands.Bot(command_prefix=bot_prefix, description="Teams & Leagues Bot!")
 
     # These would be set per channel, not sure how RSC would implement:
+    six_man_cat_id = 701497470101749850
     bot_input = 512343431213875203
     waiting_room_voice_id = 701497581942865961
-    home_voice_id = 701498135813423166
-    away_voice_id = 701498234941603900
 
     @bot.event
     async def on_ready():
@@ -46,23 +62,28 @@ def run():
     async def start_six_mans(context):
         player_pool = bot.get_channel(waiting_room_voice_id).members
 
-        if len(player_pool) < 6 and False:
-            await context.send("Not enough players.")
-        else:
-            num_players = len(player_pool) if len(player_pool) < 6 else 6
-            for i in range(0, num_players):
-                player_pool = bot.get_channel(waiting_room_voice_id).members
-                player = random.choice(player_pool)
-                room = [home_voice_id, away_voice_id][i % 2]
-                await player.move_to(bot.get_channel(room))
+        six_mans_category = bot.get_channel(six_man_cat_id)
+        waiting_room = bot.get_channel(waiting_room_voice_id)
+        home_voice = await six_mans_category.create_voice_channel(name="Orange Team")
+        away_voice = await six_mans_category.create_voice_channel(name="Blue Team")
+
+        home_team = SixManTeam(waiting_room, home_voice)
+        away_team = SixManTeam(waiting_room, away_voice)
+
+        global game
+        game = SixManGame(home=home_team, away=away_team)
+
+        num_players = len(player_pool) if len(player_pool) < 6 else 6
+        for i in range(0, num_players):
+            player_pool = bot.get_channel(waiting_room_voice_id).members
+            player = random.choice(player_pool)
+            team = [home_team, away_team][i % 2]
+            await team.add_player(player)
 
     @bot.command(name="game_over", aliases=["gameover", "gg", "ggs", "done"], brief="Regroup Teams",
                  help="Moves all 6-man players back into the waiting room", pass_context=True)
     async def game_over(context):
-        for room in [away_voice_id, home_voice_id]:
-            player_pool = bot.get_channel(room).members
-            for player in player_pool:
-                await player.move_to(bot.get_channel(waiting_room_voice_id))
+        await game.teardown()
 
     bot.run(token)
 
